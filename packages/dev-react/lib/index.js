3c17a77d010e5b6750547749b5334df23499c219
WuJunFeng <xiguashuang5@gmail.com>
2020-12-17 16:55:39 +0800

    ignore

---
+++ packages/dev-react/lib/index.js
@@ -0,0 +1,54 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true,
+});
+exports.Main = void 0;
+
+var _Config = _interopRequireDefault(require('./Config'));
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
+
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError('Cannot call a class as a function');
+  }
+}
+
+function _defineProperties(target, props) {
+  for (var i = 0; i < props.length; i++) {
+    var descriptor = props[i];
+    descriptor.enumerable = descriptor.enumerable || false;
+    descriptor.configurable = true;
+    if ('value' in descriptor) descriptor.writable = true;
+    Object.defineProperty(target, descriptor.key, descriptor);
+  }
+}
+
+function _createClass(Constructor, protoProps, staticProps) {
+  if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+  if (staticProps) _defineProperties(Constructor, staticProps);
+  return Constructor;
+}
+
+var Main = /*#__PURE__*/ (function() {
+  function Main() {
+    _classCallCheck(this, Main);
+  }
+
+  _createClass(Main, [
+    {
+      key: 'init',
+      value: function init() {
+        var config = new _Config.default();
+        return config;
+      },
+    },
+  ]);
+
+  return Main;
+})();
+
+exports.Main = Main;
